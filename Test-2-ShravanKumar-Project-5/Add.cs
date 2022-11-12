f68916f45a5195081956343a7c44dff90b3cab23
ShravanMCAnu <bskmc850@gmail.com>
2022-11-12 14:40:46 +0530

    Add project files.

---
+++ Test-2-ShravanKumar-Project-5/Add.cs
@@ -0,0 +1,37 @@
+﻿namespace Test_2_ShravanKumar_Project_5
+{
+    internal class Add
+    {
+        public int AddingTwoNumbers(int _number1, int _number2)
+        {
+            return _number1 + _number2;
+        }
+        public int AddingTwoNumbers(int _number1, int _number2, int _number3)
+        {
+            return _number1 + _number2 + _number3;
+        }
+        public double AddingTwoNumbers(double _number1, int _number2)
+        {
+            return _number1 + _number2;
+        }
+        public double AddingTwoNumbers(int _number1, double _number2)
+        {
+            return _number1 + _number2;
+        }
+        public int AddingTwoNumbers(int _number2)
+        {
+            int number1 = _number2;
+            return number1 + _number2;
+        }
+        public double AddingTwoNumbers(double _number1, double _number2)
+        {
+            return _number1 + _number2;
+        }
+        public double AddingTwoNumbers(double _number2)
+        {
+            double number1 = _number2;
+            return number1 + _number2;
+        }
+
+    }
+}
---
+++ Test-2-ShravanKumar-Project-5/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Test_2_ShravanKumar_Project_5;
+
+Add objAdd = new Add();
+Console.WriteLine("Add of two Numbers with two integer data type parameters:" + objAdd.AddingTwoNumbers(1, 2));
+
+Console.WriteLine("Add of two Numbers  with three integer data type parameters:" + objAdd.AddingTwoNumbers(1, 2, 3));
+
+Console.WriteLine("Add of two Numbers with one integer , one double data type parameters:" + objAdd.AddingTwoNumbers(1.566, 2));
+
+Console.WriteLine("Add of two Numbers with one double , one integer data type parameters:" + objAdd.AddingTwoNumbers(1, 2.857));
+
+Console.WriteLine("Add of two Numbers one integer data type parameter:" + objAdd.AddingTwoNumbers(1));
+
+Console.WriteLine("Add of two Numbers  one double data type parameter:" + objAdd.AddingTwoNumbers(2.586));
+
+Console.WriteLine("Add of two Numbers with two double data type parameters:" + objAdd.AddingTwoNumbers(2.586, 1.856));
+
